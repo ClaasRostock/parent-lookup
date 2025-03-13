0196f5371babc77eda7019a23243306ecb1bfede
Claas <claas.rostock@dnv.com>
2025-03-13 22:43:50 +0100

    pyright

--- tests/lookup/custom_base_module.py
+++ tests/lookup/custom_base_module.py
@@ -13,6 +13,6 @@ class CustomBase:
     # TODO @ClaasRostock: Change return type to Self once Python 3.10 support is dropped.
     #      ClaasRostock, 2025-02-03
     def __new__(cls) -> Self:
-        instance = super().__new__(cls)  # pyright: ignore[reportArgumentType]
+        instance = super().__new__(cls)
         lookup_registry.register_parent(instance)
         return instance
